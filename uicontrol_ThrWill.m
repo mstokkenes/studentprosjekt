856c80c2627545b2717e91a2c29d4def14f179e2
mstokkenes <77609740+mstokkenes@users.noreply.github.com>
2021-04-27 20:43:51 +0200

    Add files via upload

--- uicontrol_ThrWill.m
+++ uicontrol_ThrWill.m
@@ -1,45 +1,46 @@
 function uicontrol_ThrWill
 
 f = figure;
-p = uipanel(f,'Position',[0.1 0.6 0.6 0.2]);
-testf = uicontrol('Style', 'pushbutton', 'String', 'OK',...
-            'Fontsize', 14, 'Fontweight', 'b',...
-            'Position', [90 10 70 40], 'Callback',...
-            {@OK_button_Callback});
+p = uipanel(f,'Position',[0.125 0.75 0.75 0.2]);
     
-    setSignalTxt = uicontrol(p, 'Style', 'text',...
-                                'String', 'Wavelength',...
-                                'Position', [20 40 60 20]);
-    setSignalBox = uicontrol(p, 'Style', 'edit',...
-                                'Callback', @setmap);
-    setOPOpwrTxt = uicontrol(p, 'Style', 'text',...
-                                'String', 'OPO power',...
-                                'Position', [100 40 60 20]);
-    setOPOpwrBox = uicontrol(p, 'Style', 'edit',...
-                                'Position', [100 20 60 20],...
-                                'Callback', @setmap);
-    setIRpwrTxt  = uicontrol(p, 'Style', 'text',...
-                                'String', 'IR power',...
-                                'Position', [180 40 60 20]);
-    setIRpwrBox  = uicontrol(p, 'Style', 'edit',...
-                                'Position', [180 20 60 20],...
-                                'Callback', @setmap);
-    setDelayTxt  = uicontrol(p, 'Style', 'text',...
-                                'String', 'Delay',...
-                                'Position', [260 40 60 20]);    
-    setDelayBox  = uicontrol(p, 'Style', 'edit',...
-                                'Position', [260 20 60 20],...
-                                'Callback', @setmap);
+    function addTextBox(pos, title, unit)
+        titleText = uicontrol(p, 'Style', 'text', 'String', title, 'FontWeight', 'bold');
+        titlewidth = titleText.Extent(3);
+        titleText.Position = [pos 40 titlewidth 20];
         
-    function setmap(hObj, event) %#ok<INUSL>
+        unitText = uicontrol(p, 'Style', 'text', 'String', unit);
+        unitWidth = unitText.Extent(3);
+        unitText.Position = [pos+62 15 unitWidth 20];
+        
+        uicontrol(p, 'Style', 'edit',...
+                     'Position', [pos 20 60 20],...
+                     'Callback', @setWavelength);
+                 
+    % legg til tekstfelt
+    addTextBox(20, 'Wavelength', 'nm')
+    addTextBox(120, 'OPO power', 'mW')
+    addTextBox(220, 'IR power', 'mW')
+    addTextBox(320, 'Delay', 'fs')
+        
+    end
+
+    % test for å lage plottevindu
+    axes('Position', [0.1,0.1,0.8,0.6])
+    x = linspace(0,1);
+    plot(x, sin(2*pi*x))
+
+    
+    % skal bestemme hva som skjer når brukeren har lagt inn input 
+    function setWavelength(hObj, event) %#ok<INUSL>
         
     thr = get(hObj);
     a = char(thr.String);
     b = str2double(a);
-    if b > 1000
-        warndig('Feil input!')
-        
-    end
+    % hvis b er utenfor gyldig intervall
+        % warndig('Feil input!')
+    % else
+        % sett verdi for laser (send data)  
+    %end
     
     end
 
